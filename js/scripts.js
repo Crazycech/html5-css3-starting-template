a29337cf4af67bb38fe56e57d8e2218515e5bad0
marvantes <m.tesar@koderhtml.cz>
2015-09-16 20:34:47 +0200

    Adding files
    Filling the files of the main template

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+/*
+wow = new WOW({//basic initiation of wow plugin
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true        // default
+})wow.init();
+
+*/ 
+ 
+
+
+/*
+$( document ).ready(function() { //when document is ready
+    
+    $('nav').slicknav({ //basic usage of slicknav menu
+        label: '',
+	prependTo:'.mobile-menu'
+    });
+    
+    $(".toTop").click(function(){//basic usage of scroll-top plugin
+
+        $("body").scrollTo(0,'slow',{axis:'y'});
+
+    });
+
+    $("#swipe").swipe({ //basic usage of swipe plugin for touch devices
+      swipeLeft:function(event, direction, distance, duration, fingerCount) {
+        //This only fires when the user swipes left
+      },
+      swipeRight:function(event, direction, distance, duration, fingerCount) {
+        //This only fires when the user swipes right
+      }
+    });
+    
+    
+});
+*/
+
+
+/*
+$(window).scroll(function(){ //on window scroll function
+        
+});
+*/
